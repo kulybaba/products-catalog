77ddaac6d1ac7d119299b81ff3193e1be92596b2
Petro Kulybaba <petro.kulybaba@ekreative.com>
2019-05-14 13:26:48 +0300

    products list

---
+++ src/Controller/ProductController.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\Product;
+use Knp\Component\Pager\PaginatorInterface;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Routing\Annotation\Route;
+
+/**
+ * @Route("/products")
+ */
+class ProductController extends AbstractController
+{
+    /**
+     * @param Request $request
+     * @param PaginatorInterface $paginator
+     *
+     * @return \Symfony\Component\HttpFoundation\Response
+     *
+     * @Route("")
+     */
+    public function list(Request $request, PaginatorInterface $paginator)
+    {
+        $products = $this->getDoctrine()->getRepository(Product::class)->getAll();
+
+        return $this->render('product/list.html.twig', [
+            'products' => $paginator->paginate(
+                $products,
+                $request->query->getInt('page', 1),
+                $this->getParameter('page_range')
+            ),
+        ]);
+    }
+}
--- src/Repository/ProductRepository.php
+++ src/Repository/ProductRepository.php
@@ -19,32 +19,8 @@ class ProductRepository extends ServiceEntityRepository
         parent::__construct($registry, Product::class);
     }
 
-    // /**
-    //  * @return Product[] Returns an array of Product objects
-    //  */
-    /*
-    public function findByExampleField($value)
+    public function getAll()
     {
-        return $this->createQueryBuilder('p')
-            ->andWhere('p.exampleField = :val')
-            ->setParameter('val', $value)
-            ->orderBy('p.id', 'ASC')
-            ->setMaxResults(10)
-            ->getQuery()
-            ->getResult()
-        ;
+        return $this->createQueryBuilder('p')->getQuery();
     }
-    */
-
-    /*
-    public function findOneBySomeField($value): ?Product
-    {
-        return $this->createQueryBuilder('p')
-            ->andWhere('p.exampleField = :val')
-            ->setParameter('val', $value)
-            ->getQuery()
-            ->getOneOrNullResult()
-        ;
-    }
-    */
 }
