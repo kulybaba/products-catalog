a97080acacca4e85b67bdc13808864327c45b9d5
Petro Kulybaba <petro.kulybaba@ekreative.com>
2019-05-16 19:10:34 +0300

    updating comments

--- src/Controller/CommentController.php
+++ src/Controller/CommentController.php
@@ -2,18 +2,45 @@
 
 namespace App\Controller;
 
+use App\Entity\Comment;
+use App\Form\CommentType;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
 
+/**
+ * @Route("/comments")
+ */
 class CommentController extends AbstractController
 {
     /**
-     * @Route("/comment", name="comment")
+     * @param Request $request
+     * @param Comment $comment
+     * @param int $productId
+     *
+     * @return \Symfony\Component\HttpFoundation\RedirectResponse|\Symfony\Component\HttpFoundation\Response
+     *
+     * @Route("/{id}/edit")
      */
-    public function list()
+    public function edit(Request $request, Comment $comment)
     {
-        return $this->render('comment/index.html.twig', [
-            'controller_name' => 'CommentController',
+        $form = $this->createForm(CommentType::class, $comment);
+        $form->handleRequest($request);
+        if ($form->isSubmitted() && $form->isValid()) {
+            $em = $this->getDoctrine()->getManager();
+            $em->persist($comment);
+            $em->flush();
+
+            $this->addFlash('notice', 'Comment updated!');
+
+            return $this->redirectToRoute('app_product_view', [
+                'id' => $comment->getProduct()->getId(),
+            ]);
+        }
+
+        return $this->render('comment/edit.html.twig', [
+            'form' => $form->createView(),
+            'productId' => $comment->getProduct()->getId(),
         ]);
     }
 }
